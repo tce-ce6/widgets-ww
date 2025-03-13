9368fd448b70c3eed82020970c568b1b3d977399
Shyam <shyam@thewhitewarrior.com>
2025-03-13 13:43:04 +0530

    folder structure fix

--- ElapsedTime/js/app.js
+++
@@ -1,6 +0,0 @@
-// Initialize the application
-document.addEventListener('DOMContentLoaded', () => {
-    const model = new TimeModel();
-    const view = new TimeView();
-    const controller = new TimeController(model, view,'app.js file calling ');
-}); 
