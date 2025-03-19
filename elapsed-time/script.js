a7e175426d9ca53f476486dbd8d0ff7216d48630
Shyam <shyam@thewhitewarrior.com>
2025-03-19 18:42:17 +0530

    time line on ticks fixed

--- elapsed-time/script.js
+++ elapsed-time/script.js
@@ -135,7 +135,7 @@ class TimeModel {
             minDuration: 30,
             maxDuration: 180,
             minuteStep: 5, // 5-minute increments
-            hoursInTimeline: 10,
+            hoursInTimeline: 13,
             minutesPerHour: 60,
             timelineStartHour: 1 // Timeline always starts at 1 PM
         };
@@ -256,7 +256,7 @@ class TimeModel {
 
         if (hours >= 12) {
             if (hours > 12) adjustedHours = hours - 12;
-            if (hours >= 24) {
+            if (hours >= 12) {
                 period = 'AM';
                 if (adjustedHours > 12) adjustedHours -= 12;
             }
