15e1a4eb20f3133c709fc6a4edb6afcbda0eace4
tce-nitinc <v_nitinch@tataclassedge.com>
2025-12-30 20:42:37 +0530

    listing page

--- widget-listing/script/script.js
+++ widget-listing/script/script.js
@@ -217,7 +217,7 @@ const WIDGET_DATA = [
     link: "https://ce-predev-school.devstudi.com/mathwidgets/shyam/wg49-Temperature-conversions/index.html",
     imagePath: "./assets/wg-49.png",
     creators: "sh-di-49",
-    status: "in-review",
+    status: "closed",
   },
   {
     name: "Hard and Soft Ferromagnetic Materials",
@@ -371,14 +371,14 @@ const WIDGET_DATA = [
     link: "https://ce-predev-school.devstudi.com/mathwidgets/shyam/wg57-role-of-fuse/index.html",
     imagePath: "./assets/wg-57.png",
     creators: "sh-di-57",
-    status: "in-review",
+    status: "closed",
   },
   {
     name: "Logic gates: AND, OR and NOT",
     link: "https://ce-predev-school.devstudi.com/mathwidgets/shyam/wg58-logic-gates/index.html",
     imagePath: "./assets/wg-58.png",
     creators: "sh-ra-58",
-    status: "in-review",
+    status: "closed",
   },
   {
     name: "Know Your Apparatus",
@@ -428,7 +428,7 @@ const WIDGET_DATA = [
     link: "https://ce-predev-school.devstudi.com/mathwidgets/shyam/wg62-regular-and-difused-reflection/index.html",
     imagePath: "./assets/wg-62.png",
     creators: "sh-ra-62",
-    status: "in-review",
+    status: "closed",
   },
   {
     name: "Word Discovery Adventure",
@@ -456,7 +456,7 @@ const WIDGET_DATA = [
     link: "https://ce-predev-school.devstudi.com/mathwidgets/nitin/wg59-factors-affecting-resistance/index.html",
     imagePath: "./assets/wg-59.png",
     creators: "ni-ra-59",
-    status: "in-review",
+    status: "closed",
   },
   
 
