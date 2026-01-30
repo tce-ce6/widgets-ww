db93feff969fe9e1ead69feb38ea79d0d8da08ab
tce-nitinc <v_nitinch@tataclassedge.com>
2026-01-30 19:52:01 +0530

    wg75 changes

---
+++ wg75-reading-slopes/js/script.js
@@ -0,0 +1,63 @@
+const data =
+{
+  "Gentle Slope": {
+      "title": "Gentle Slope",
+      "Insights": "When a landform has a very small gradient or angle, the slope is gentle, and its contour lines are widely spaced.",
+      "counter lines" :[
+        "../assets/gentle-slope-1.svg",
+        "../assets/gentle-slope-2.svg",
+        "../assets/gentle-slope-3.svg",
+        "../assets/gentle-slope-4.svg",
+        "../assets/gentle-slope-5.svg",
+        "../assets/gentle-slope-6.svg",
+      ]
+  },
+  "Steep Slope": {
+      "title": "Steep Slope",
+      "Insights": "When the degree or angle of slope of a feature is high, and the contours are closely spaced, they indicate a steep slope. ",
+      "counter lines" :[
+        "../assets/steep-slope-1.svg",
+        "../assets/steep-slope-2.svg",
+        "../assets/steep-slope-3.svg",
+        "../assets/steep-slope-4.svg",
+        "../assets/steep-slope-5.svg",
+        "../assets/steep-slope-6.svg",
+      ]
+  },
+  "Convex Slope": {
+      "title": "Convex Slope",
+      "Insights": "The slope is gentle at the base and gradually becomes steeper toward the summit. The contour lines are widely spaced at lower elevations and progressively closer together at higher elevations. ",
+      "counter lines" :[
+        "../assets/convex-slope-1.svg",
+        "../assets/convex-slope-2.svg",
+        "../assets/convex-slope-3.svg",
+        "../assets/convex-slope-4.svg",
+        "../assets/convex-slope-5.svg",
+        "../assets/convex-slope-6.svg",
+      ]
+  },
+  "Concave Slope": {
+      "title": "Concave Slope",
+      "Insights": "A concave slope is steep at the base and gradually becomes gentler toward the top. The contour lines are closely spaced at lower levels and increasingly wider apart at higher levels. ",
+      "counter lines" :[
+        "../assets/concave-slope-1.svg",
+        "../assets/concave-slope-2.svg",
+        "../assets/concave-slope-3.svg",
+        "../assets/concave-slope-4.svg",
+        "../assets/concave-slope-5.svg",
+        "../assets/concave-slope-6.svg",
+      ]
+  },
+  "Irregular Slope": {
+      "title": "Irregular Slope",
+      "Insights": "A slope where contour spacing alternates between wide and close intervals, indicating an uneven gradient with successive gentle and steep sections along the slope.",
+      "counter lines" :[
+        "../assets/irregular-slope-1.svg",
+        "../assets/irregular-slope-2.svg",
+        "../assets/irregular-slope-3.svg",
+        "../assets/irregular-slope-4.svg",
+        "../assets/irregular-slope-5.svg",
+        "../assets/irregular-slope-6.svg",
+      ]
+  },
+}
