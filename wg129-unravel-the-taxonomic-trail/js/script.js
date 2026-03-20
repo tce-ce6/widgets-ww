feb4b5973a6ede3164106bad19c167dd0abffb34
aditya <aditya@thewhitewarrior.com>
2026-03-20 12:36:13 +0530

    fix wg129

--- wg129-unravel-the-taxonomic-trail/js/script.js
+++ wg129-unravel-the-taxonomic-trail/js/script.js
@@ -819,7 +819,10 @@ document.addEventListener("DOMContentLoaded", () => {
         nameEl.querySelector('tspan').textContent = org.name;
       }
       if (explNameEl) explNameEl.textContent = org.name;
-      if (explLevelEl) explLevelEl.textContent = `${currentLevelName} ${currentTaxonName}`;
+      if (explLevelEl) {
+        let text = `${currentLevelName} ${currentTaxonName}`;
+        explLevelEl.innerHTML = text.replace(/(margarita|Felis)/ig, '<tspan font-style="italic">$1</tspan>');
+      }
     }
 
     const showAnswerEl = document.getElementById('show_answer');
