e47b401834f5f45ff20e8fed1075aebdaf20410d
aditya <aditya@thewhitewarrior.com>
2026-03-20 12:43:20 +0530

    wg 129 fix

--- wg129-unravel-the-taxonomic-trail/js/script.js
+++ wg129-unravel-the-taxonomic-trail/js/script.js
@@ -821,7 +821,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (explNameEl) explNameEl.textContent = org.name;
       if (explLevelEl) {
         let text = `${currentLevelName} ${currentTaxonName}`;
-        explLevelEl.innerHTML = text.replace(/(margarita|Felis)/ig, '<tspan font-style="italic">$1</tspan>');
+        explLevelEl.innerHTML = text.replace(/(margarita|Felis|Spirogyra|longata)/ig, '<tspan font-style="italic">$1</tspan>');
       }
     }
 
